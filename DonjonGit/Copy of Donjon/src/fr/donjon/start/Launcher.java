b5438ca3220715a7d95c8e906f2ebd7ea02d1e3e
batlac <baptiste.lac@insa-lyon.fr>
2015-04-16 23:09:04 +0200

    Gestion des erreurs ds le launcher

--- DonjonGit/Copy of Donjon/src/fr/donjon/start/Launcher.java
+++ DonjonGit/Copy of Donjon/src/fr/donjon/start/Launcher.java
@@ -19,55 +19,62 @@ import fr.donjon.utils.Orientation;
 public class Launcher extends JFrame implements EcouteurClavier{
 
 	JPanel panActuel;
-	
+
 	GamePanel game;	//LE JPanel dessinant le jeu (GamePanel)
 	JPanel menu;	//Le JPanel dessinant le menu
-	
+
 	/**
 	 * 
 	 */
 	public Launcher(){
-		
+
 		game = new JeuLineaireBlac();
 		menu = new EcranAccueil(this);
-		
-		panActuel = menu;
-		
-	    add(panActuel);
-		
-	    
+
+		goToMenu();
+
 		pack();
-        setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        
-        
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+
+
 	}
-	
+
 	/**
 	 * Affiche le jeu
 	 */
 	public void startGame(){
 		this.game.stopGame();
-		this.remove(panActuel);
+		try{
+			this.remove(panActuel);
+		}
+		catch (NullPointerException e){
+
+		}
 		this.panActuel = game;
 		this.add(panActuel);
 		this.pack();
 	}
-	
+
 	/**
 	 * Affiche le menu
 	 */
 	public void goToMenu(){
 		this.game.stopGame();
-		this.remove(panActuel);
+		try{
+			this.remove(panActuel);
+		}
+		catch (NullPointerException e){
+
+		}
 		this.panActuel = menu;
 		this.add(panActuel);
 		this.pack();
-		
+
 	}
-	
-	
+
+
 	/**
 	 * @param args
 	 */
@@ -76,9 +83,9 @@ public class Launcher extends JFrame implements EcouteurClavier{
 		Launcher launch = new Launcher();
 	}
 
-	
+
 	///////////////////////////////////////////////////////////////
-	
+
 	@Override
 	public void attaque(Orientation o) {
 		// TODO Auto-generated method stub
@@ -94,20 +101,20 @@ public class Launcher extends JFrame implements EcouteurClavier{
 	@Override
 	public void deplacement(Orientation o) {
 		// TODO Auto-generated method stub
-		
+
 		game.gestionnaire.listeSalles.getCurrent().getSalle().deplacement(o);
 	}
 
 	@Override
 	public void utiliseObjet(int reference) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void togglePause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
