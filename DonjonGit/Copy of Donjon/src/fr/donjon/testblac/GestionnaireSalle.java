4eae9cdd0a8807abd5e6d6b14218dfc9832500c5
Patrick <Patrick@10.44.3.40>
2015-05-11 22:57:47 +0200

    Javadoc. Créé des mutateurs/accesseurs qui vont bien. Peut-être d'autres à venir ;)

--- DonjonGit/Copy of Donjon/src/fr/donjon/testblac/GestionnaireSalle.java
+++ DonjonGit/Copy of Donjon/src/fr/donjon/testblac/GestionnaireSalle.java
@@ -14,24 +14,47 @@ import fr.donjon.utils.Vecteur;
 public abstract class GestionnaireSalle implements EcouteurChangementSalle, EcouteurClavier{
 
 
-	Salle[][] smap;
+	Salle[][] smap;			//le tableau de salles du donjon
 
-	Salle sActuelle;
+	Salle sActuelle;		//la salle dans laquelle le héros se trouve
 
-	Vecteur position;
+	Vecteur position;		//le vecteur contenant les coordonnées de la salle actuelle
 
-	Vecteur centreCamera;
+	Vecteur centreCamera; 	//vecteur contenant l'emplacement du personnage dans la salle.
+							//Il est utile quand on cherche à peindre la salle.
 
+	/**
+	 * Constructeur initialisant les dimensions du donjon
+	 * @param w la largeur du donjon (nombres de salles sur axe horizontal)
+	 * @param h la hauteur du donjon (nombres de salles sur axe vertical)
+	 */
 	public GestionnaireSalle(int w, int h) {
 		this.smap = new Salle[w][h];
 		this.position = new Vecteur(0,0);
 		this.centreCamera = new Vecteur(0,0);
 	}
 
+	/**
+	 * Constructeur initialisant une première salle et donnant les dimensions du donjon.
+	 * Par défaut la première salle est placée en haut à gauche du tableau.
+	 * @param s la première salle
+	 * @param w la largeur du donjon (nombres de salles sur axe horizontal)
+	 * @param h la hauteur du donjon (nombres de salles sur axe vertical)
+	 */
 	public GestionnaireSalle(Salle s, int w, int h){
 		this(s,0,0,w,h);
 	}
 
+	
+	/**
+	 * Constructeur initialisant une première salle et donnant les dimensions du donjon
+	 * La première salle est placée aux coordonnées sx et sy données en paramètres
+	 * @param s la première salle du donjon
+	 * @param sx position x dans le tableau de la première salle
+	 * @param sy position y dans le tableau de la première salle
+	 * @param w la largeur du donjon (nombres de salles sur axe horizontal)
+	 * @param h la hauteur du donjon (nombres de salles sur axe vertical)
+	 */
 	public GestionnaireSalle(Salle s, int sx, int sy, int w, int h){
 		this(w,h);
 		smap[sx][sy] = s;
@@ -39,43 +62,77 @@ public abstract class GestionnaireSalle implements EcouteurChangementSalle, Ecou
 		this.position = new Vecteur(sx,sy);
 	}
 
+	/**
+	 * Méthode raffraichissant la salle actuelle
+	 * @param t le temps qui s'est écoulé depuis le début du jeu, paramètre nécessaire aux animations et autres.
+	 */
 	public void update (long t){
 
-		sActuelle.update(t);
+		sActuelle.update(t);	//On raffarichit uniquement la salle actuelle, inutile de faire bouger les ennemis dans tout le donjon.
 
 		centreCamera.setLocation(sActuelle.hero.image.x + sActuelle.hero.image.width/2,
-				sActuelle.hero.image.height/2 + sActuelle.hero.image.y);
+				sActuelle.hero.image.height/2 + sActuelle.hero.image.y);		//On recentre le vecteur centre caméra sur la position du personnage.
 	}
 
 
 	public boolean changementSalle(Vecteur dir){
 		
-		Vecteur npos = position.ajoute(dir);
+		Vecteur npos = position.ajoute(dir); //les coordonnées de la prochaine salle dans le tableau.
 
-		if( !(npos.x >= 0 && npos.x < smap.length && npos.y >= 0  && npos.y < smap[0].length) )return false;
+		if( !(npos.x >= 0 && npos.x < smap.length && npos.y >= 0  && npos.y < smap[0].length) )return false; //On tombe en dehors du tableau de salle, on renvoi false
 
-		if(smap[(int)npos.x][(int)npos.y] != null ){
-			setSActuelle( smap[(int)npos.x][(int)npos.y] , dir );
+		if(this.getSalleAt(npos) != null ){
+			setSActuelle(npos);
 		}
 		else{
-			Salle s = fournirNouvelleSalle((int)npos.x, (int)npos.y);
-			setSActuelle( s, dir);
-			smap[(int)npos.x][(int)npos.y] = s;
+			smap[(int)npos.x][(int)npos.y] = fournirNouvelleSalle((int)npos.x, (int)npos.y); //On créé une nouvelle salle et on la met dans le tableau
+			setSActuelle(npos);
+			
 		}
 		
-		return true;
-
+		return true;		//On a bien changé de salle, on renvoie true
 	}
 
-	public void setSActuelle(Salle s, Vecteur nPos){
-		sActuelle = s;
-		position = position.ajoute(nPos);
+	/**
+	 * Changes the current room to the one directly located on the (Orientation) of it in the matrix
+	 * @param o the orientation from the current room to the next one in the matrix.
+	 */
+	public void setSActuelle(Orientation o){
+		position = position.ajoute(Orientation.getUnitVector(o));
+		setSActuelle(position);
+	}
+	
+	/**
+	 * Change la salle actuelle à celle stuée dans le tableau aux coordonnées données dans le vecteur
+	 * @param nouvelleSalle les coordonnées dans le tableau de la nouvelle salle actuelle.
+	 */
+	private void setSActuelle(Vecteur Salle){
+		position = Salle;
+		this.setSalleActuelleOn(position);
 	}
 
+	/**
+	 * donne la salle située à la position dans le donjon donnée par le vecteur.
+	 * @param v
+	 * @return
+	 */
+	private Salle getSalleAt(Vecteur v){
+		return smap[(int) v.x][(int) v.y];
+	}
+	
 	public abstract Salle fournirNouvelleSalle(int x, int y);
 
 	
 
+	/**
+	 * change la salle actuelle pour celle aux coordonnées indiqués.
+	 * @param v
+	 */
+	private void setSalleActuelleOn(Vecteur v){
+		sActuelle = smap[(int)v.x][(int)v.y];
+	}
+	
+	
 	//MUT
 	
 	/**
