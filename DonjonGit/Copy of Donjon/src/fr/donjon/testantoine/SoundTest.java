d9c20ba91a83c21e65928fcf6e6efdd64d6c2d05
chwat <chwat@acces0745.res.insa-lyon.fr>
2015-05-11 22:22:54 +0200

    bis repetita

---
+++ DonjonGit/Copy of Donjon/src/fr/donjon/testantoine/SoundTest.java
@@ -0,0 +1,25 @@
+package fr.donjon.testantoine;
+
+import java.io.*;
+import javax.sound.sampled.*;
+
+public class SoundTest{
+	
+	public static Clip clip;
+	
+	 public static void main(String[] args) {
+		 try
+		    {
+		        clip = AudioSystem.getClip();
+		        clip.open(AudioSystem.getAudioInputStream(new File("/Users/chwat/git/Donjon_SCAN_GIT/DonjonGit/Copy of Donjon/Ressources/Sounds/RPG Theme.wav")));
+		    }
+		    catch (Exception exc)
+		    {
+		        exc.printStackTrace(System.out);
+		    }
+		 clip.setFramePosition(0);
+         clip.loop(0);
+         clip.start();
+	   }
+	
+}
