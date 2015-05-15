f7a709849e89e05baada1598cb2c50d0deeac10e
Patrick <Patrick@10.44.2.202>
2015-05-15 12:21:04 +0200

    minor change ;)

--- DonjonGit/Copy of Donjon/src/fr/donjon/testblac/gestionnairePatrickBasique.java
+++ DonjonGit/Copy of Donjon/src/fr/donjon/testblac/gestionnairePatrickBasique.java
@@ -12,12 +12,23 @@ public class gestionnairePatrickBasique extends GestionnaireSalle {
 	
 	/**
 	 * handles a new donjon. This one is just a grid of size w*h
+	 * the first room is by default placed in 0;0 coordinates
 	 * @param w width of the donjon
 	 * @param h height of the donjon
 	 */
 	public gestionnairePatrickBasique(int w, int h) {
-		
-		super(new SalleQuatre(new Heros(0, 0), Salle.addWalls(MapGenerator.randomForet(SALLEWIDTH, SALLEHEIGHT))),w, h);
+		this(w, h,0,0);
+	}
+
+	/**
+	 * Constructor
+	 * @param w width of the donjon
+	 * @param h height of the donjon
+	 * @param sX x position of the 1st room
+	 * @param sY y position of the 1st room
+	 */
+	public gestionnairePatrickBasique(int w, int h, int sX, int sY){
+		super(new SalleQuatre(new Heros(0, 0), Salle.addWalls(MapGenerator.randomForet(SALLEWIDTH, SALLEHEIGHT))),sX,sY,w, h);
 		
 		this.sActuelle.hero.setLocation(new Vecteur(1,1));
 		this.sActuelle.createPorteSalleVoisines(smap);
@@ -28,7 +39,6 @@ public class gestionnairePatrickBasique extends GestionnaireSalle {
 		
 		this.sActuelle.activerLesPortes(false);
 	}
-
 
 	@Override
 	public void fournirNouvelleSalle(Vecteur position, Link l, Salle[][] smap) {
